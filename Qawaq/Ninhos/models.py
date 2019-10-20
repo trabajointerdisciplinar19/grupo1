1a42023c403a2463a252c77b8c781cc7155e5063
Jaime Torres <jaimetr97@gmail.com>
2019-10-20 17:08:44 -0500

    Estructura y Base de datos

---
+++ Qawaq/Ninhos/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+# Create your models here.
+
+class Ninho(models.Model):
+    DNI = models.PositiveIntegerField(primary_key = True)
+    P_Nombre = models.CharField(max_length = 20)
+    S_Nombre = models.CharField(max_length = 20)
+    Apellido_P = models.CharField(max_length = 20)
+    Apellido_M = models.CharField(max_length = 20)
+    fecha_nacimiento = models.DateField()
+    temas_interes = models.TextField()
+
+class Usuario(models.Model):
+
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    nivel_lectura = models.DecimalField(max_digits = 5, decimal_places = 2)
+    ninho = models.OneToOneField(Ninho,on_delete=models.PROTECT)
+
+class Material_Aprendizaje(models.Model):
+
+    titulo = models.CharField(max_length = 50)
+    dificultad = models.PositiveIntegerField()
+    tema = models.CharField(max_length = 30)
+
+
+class Libro(models.Model):
+
+    isbn = models.PositiveIntegerField()
+    genero = models.CharField(max_length = 30)
+    autor = models.TextField()
+    Material_Apren = models.ForeignKey(Material_Aprendizaje,on_delete = models.CASCADE)
+
+class Videos(models.Model):
+
+    URL = models.URLField()
+    duracion = models.DurationField()
+    Material_Aprendizaje = models.ForeignKey(Material_Aprendizaje,on_delete = models.CASCADE)
+
+class Historial(models.Model):
+    ninho = models.OneToOneField(Ninho,on_delete=models.PROTECT)
+    material_a = models.OneToOneField(Material_Aprendizaje,on_delete=models.PROTECT)
+    resultado = models.DecimalField(max_digits = 2, decimal_places = 2)
+
+
+class Evaluacion(models.Model):
+    puntuacion_total = models.DecimalField(max_digits = 2, decimal_places = 2)
+    Material_A = models.ForeignKey(Material_Aprendizaje, on_delete = models.CASCADE)
+
+class Minigame(models.Model):
+    Ubicacion = models.FilePathField()
+    Solucion = models.FilePathField()
+    Evaluacion = models.OneToOneField(Evaluacion,on_delete=models.PROTECT)
+
+class Pregunta(models.Model):
+    Preguntas = models.FilePathField()
+    Respuestas = models.FilePathField()
+    Evaluacion = models.OneToOneField(Evaluacion,on_delete=models.PROTECT)
